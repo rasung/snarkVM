778bef77dc4a7ec2b723af6f9abacc9d0c027314
fundDeveloper22 <xotjrrl@naver.com>
2024-10-17 15:11:09 +0900

    Update lib.rs

--- ledger/puzzle/src/lib.rs
+++ ledger/puzzle/src/lib.rs
@@ -145,16 +145,16 @@ impl<N: Network> Puzzle<N> {
     pub fn get_proof_target_from_partial_solution(&self, partial_solution: &PartialSolution<N>) -> Result<u64> {
         // If the proof target is in the cache, then return it.
         if let Some(proof_target) = self.proof_target_cache.write().get(&partial_solution.id()) {
-            println!("===== aaa ======= ");
+            //println!("===== aaa ======= ");
             return Ok(*proof_target);
         }
-        println!("===== bbb ======= ");
+        //println!("===== bbb ======= ");
 
         // Construct the leaves of the Merkle tree.
         let leaves = self.get_leaves(partial_solution)?;
         // Get the proof target.
         let proof_target = Self::leaves_to_proof_target(&leaves)?;
-        println!("===== ccc ======= ");
+        //println!("===== ccc ======= ");
 
         // Insert the proof target into the cache.
         self.proof_target_cache.write().put(partial_solution.id(), proof_target);
